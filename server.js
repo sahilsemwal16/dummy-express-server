f214e2c74238904ba29afa4250f2dd8f0de4def0
Sahil Semwal <126947728+sahilsemwal16@users.noreply.github.com>
2023-06-21 23:52:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', => console.log(res)
+app.get('/', => (req, res){
+    console.log(req);
+});
 
 app.listen(3000, function(){
     console.log("Server started!");

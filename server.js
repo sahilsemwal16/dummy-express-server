2fb49604f835a9cc4147347e99c2e736ca638934
Sahil Semwal <126947728+sahilsemwal16@users.noreply.github.com>
2023-07-04 00:07:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', => (req, res){
-    console.log(req);
+app.get('/', (req, res) =>{
+    res.send("<h1>This is the home page.</h1>");
 });
+app.get('/contact', (req, res) =>{
+    res.send("<h3>Contact me at sahilsemwal16@gmail.com</h3>")
+}
 
-app.listen(3000, function(){
+app.listen(3000, () =>{
     console.log("Server started!");
 });
